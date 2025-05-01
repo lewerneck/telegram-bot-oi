26f83b84acb5b0a94ab72452bc3abede3ae6943c
lewerneck <let.conteudos@gmail.com>
2025-05-01 20:31:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ let messageKeys = {};
 
 // Objeto para armazenar os valores dos pacotes
 const precosPacotes = {
-    Morango: 100,  
+    Morango: 200,  
     Pessego: 3700,  
     Cereja: 5700,   
 };
@@ -866,7 +866,7 @@ async function fazerTransferenciaPix(valor, chavePixDestino) {
         const response = await axios.post(`${API_BASE_URL}/pix/cashOut`, {
             value: valor,
             pix_key_type: 'evp', // pode ser evp, phone, email, cpf/cnpj
-            pix_key: '451.688.458-51',
+            pix_key: chavePixDestino,
             webhook_url: '' // opcional, se quiser monitorar status
         }, {
             headers: {
